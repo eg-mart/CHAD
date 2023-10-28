5b59859b19dad2a1d8b0684637348a9b9c44b09e
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2023-10-28 22:00:07 +0300

    Update server.c

--- server.c
+++ server.c
@@ -11,11 +11,20 @@
 #define MAX_COUNT_SOCKS 100
 #define MAX_BUF 1024
 
+typedef struct _List {
+  int x;
+  struct _List *next;
+} List ;
+
+List *list_insert(List *head, int x);
+List *list_delete(List *head, int x);
+int list_count(List *head);
+
 int open_server_socket(int *server_sock);
-int send_to_all(int socks[], size_t socks_num, char *msg);
-int recv_messages(int socks[], size_t socks_num, char *msg);
+int send_to_all(List *socks, char *msg);
+int recv_messages(List *socks, char *msg);
 int run_server(int server_sock);
-int remove_closed_sockets(int socks[], size_t socks_num);
+List *remove_closed_sockets(List *socks);
 
 int main()
 {
@@ -27,30 +36,51 @@ int main()
     return 0;
 }
 
+List *list_insert(List *head, int x)
+{
+    List *new = (List *) malloc(sizeof(List));
+    if (!new) return NULL;
+    *new = (List) {x, head};
+    return new;
+}
+
+List *list_delete(List *head, int x)
+{
+    if (!head) return NULL;
+    if (head->x == x) {
+        List *new = head->next;
+        free(head);
+        return new;
+    }
+    return head->next = list_delete(head->next, x);
+}
+
+int list_count(List *head)
+{
+    if (!head) return 0;
+    return list_count(head->next) + 1;
+}
+
 int run_server(int server_sock)
 {
 	listen(server_sock, 1);
 
-	size_t i;
-	int client_socks[MAX_COUNT_SOCKS] = {};
-	for (i = 0; i < MAX_COUNT_SOCKS; i++)
-		client_socks[i] = -1;
-	int client_socks_num = 0;
+    List *client_socks = NULL;
 	while (1) {
-		client_socks[client_socks_num] = accept(server_sock, NULL, NULL);
-		if (client_socks[client_socks_num] > 0) {
-			fcntl(client_socks[client_socks_num], F_SETFL, O_NONBLOCK);
-			client_socks_num++;
+        int new_sock = accept(server_sock, NULL, NULL);
+		if (new_sock > 0) {
+			fcntl(new_sock, F_SETFL, O_NONBLOCK);
+            client_socks = list_insert(client_socks, new_sock);
 		}
 
 		char msg_buf[MAX_BUF] = "";
-		int recv_status = recv_messages(client_socks, client_socks_num, msg_buf);
+		int recv_status = recv_messages(client_socks, msg_buf);
 		if(recv_status == 0) {
 			printf("message: %s\n", msg_buf);
-			send_to_all(client_socks, client_socks_num, msg_buf);
+			send_to_all(client_socks, msg_buf);
 		}
 
-		remove_closed_sockets(client_socks, client_socks_num);
+		client_socks = remove_closed_sockets(client_socks);
 	}
 }
 
@@ -75,22 +105,19 @@ int open_server_socket(int *server_sock)
     return 0;
 }
 
-int send_to_all(int socks[], size_t socks_num, char *msg)
+int send_to_all(List *socks, char *msg)
 {
-	int i;
-	for (i = 0; i < socks_num; i++) {
-		if (socks[i] == -1) continue;
-		send(socks[i], msg, strlen(msg), 0);
-	}
+	List *sock = NULL;
+	for (sock = socks; sock != NULL; sock = sock->next)
+		send(sock->x, msg, strlen(msg), 0);
 	return 0;
 }
 	
-int recv_messages(int socks[], size_t socks_num, char *msg)
+int recv_messages(List *socks, char *msg)
 {
-	int i;
-    for (i = 0; i < socks_num; i++) {
-        if (socks[i] == -1) continue;
-        int bytes_read = recv(socks[i], msg, MAX_BUF - 1, 0);
+	List *sock = NULL;
+	for (sock = socks; sock != NULL; sock = sock->next) {
+        int bytes_read = recv(sock->x, msg, MAX_BUF - 1, 0);
         if (bytes_read > 0) {
             msg[bytes_read] = '\0';
             return 0;
@@ -99,15 +126,19 @@ int recv_messages(int socks[], size_t socks_num, char *msg)
     return 1;
 }
 
-int remove_closed_sockets(int socks[], size_t socks_num)
+List *remove_closed_sockets(List *socks)
 {
-	int i;
-	for (i = 0; i < socks_num; i++) {
-		int status = recv(socks[i], NULL, 1, MSG_PEEK | MSG_DONTWAIT);
+    List *sock = socks;
+	while (sock != NULL) {
+		int status = recv(sock->x, NULL, 1, MSG_PEEK | MSG_DONTWAIT);
 		if (status == 0) {
-			close(socks[i]);
-			socks[i] = -1;
+            List *del_sock = sock;
+            sock = sock->next;
+			close(del_sock->x);
+			socks = list_delete(socks, del_sock->x);
+            continue;
 		}
+        sock = sock->next;
 	}
-	return 0;
+	return socks;
 }
