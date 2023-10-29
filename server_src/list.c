83b3ac8661447e2997f2ec19b5ef9dcd5b8e47e8
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2023-10-29 04:31:56 +0300

    add names & fix bugs

--- server_src/list.c
+++ server_src/list.c
@@ -1,28 +1,42 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "list.h"
 
-List *list_insert(List *head, int x)
+#define PIOSON_NAME "UNKNOWN"
+
+List *list_insert(List *head, int client_sock)
 {
 	List *new = (List *) malloc(sizeof(List));
 	if (!new) return NULL;
-	*new = (List) {x, head};
+	*new = (List) {client_sock, PIOSON_NAME, head};
 	return new;
 }
 
-List *list_delete(List *head, int x)
+List *list_delete(List *head, int client_sock)
 {
 	if (!head) return NULL;
-	if (head->x == x) {
+	if (head->client_sock == client_sock) {
 		List *new = head->next;
 		free(head);
 		return new;
 	}
-	return head->next = list_delete(head->next, x);
+	head->next = list_delete(head->next, client_sock);
+	return head;
 }
 
 int list_count(List *head)
 {
 	if (!head) return 0;
 	return list_count(head->next) + 1;
+}
+
+void list_print(List *head)
+{
+	if (!head) {
+		printf("\n");
+		return;
+	}
+	printf("%d ", head->client_sock);
+	list_print(head->next);
 }
--- server_src/sockets.h
+++ server_src/sockets.h
@@ -3,11 +3,6 @@
 
 #include "list.h"
 
-#define PORT 5002
-#define ADRESS "89.223.123.237"
-#define MAX_COUNT_SOCKS 100
-#define MAX_BUF 1024
-
 int open_server_socket(int *server_sock);
 int send_to_all(List *socks, char *msg);
 int recv_messages(List *socks, char *msg);
@@ -15,4 +10,4 @@ int run_server(int server_sock);
 List *remove_closed_sockets(List *socks);
 char *get_socket_name(int sock);
 
-#endif
+#endif // SOCKETS_H
